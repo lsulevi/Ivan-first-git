a7f17e34b01a239a89602e65b6778068cfd1a6ac
lsulevi <qqqqban@gmail.com>
2023-02-25 00:12:21 +0800

    test

--- SpringBoot/src/main/java/com/example/demo/sss.java
+++ SpringBoot/src/main/java/com/example/demo/sss.java
@@ -7,9 +7,24 @@ import java.util.Hashtable;
 public class sss{
 	private String sThreadName;
 	public static void main(String[] args) throws Exception{
-		Integer.valueOf("");
-		Boolean.valueOf("");
-		Short.valueOf("");
+		String a = " ";
+		String b = "*";
+		String str = "";
+		int nu = 5;
+		int x = 1;
+		for(int f = 1 ; f<= 5 ; f++) {
+			for(int i = 1 ; i<= nu ; i++) {
+				str += a;
+			}
+
+			for(int k = 1 ; k<= x ; k++) {
+				str += b;
+			}
+			nu--;
+			x ++;
+			System.out.println(str);
+			str = "";
+		}
 
 	}
 }
